1a73e0bafb88021febcbd04b1427635e05e1a272
Cascode6 <caseylfaist@gmail.com>
2019-04-30 08:46:46 -0700

    test no env read on Heroku

--- dynowiki/settings/heroku.py
+++ dynowiki/settings/heroku.py
@@ -9,8 +9,8 @@ env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
 )
-# reading .env file
-environ.Env.read_env()
+# # reading .env file
+# environ.Env.read_env()
 
 # False if not in os.environ
 DEBUG = env('DEBUG')
