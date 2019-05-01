77c924dfde8a2037515776db370a3e3bad6d8d41
Cascode6 <caseylfaist@gmail.com>
2019-05-01 00:19:16 -0700

    add apps for wiki media routing

--- dynowiki/settings/local.py
+++ dynowiki/settings/local.py
@@ -47,7 +47,9 @@ INSTALLED_APPS = [
     'wiki.plugins.attachments.apps.AttachmentsConfig',      #
     'wiki.plugins.notifications.apps.NotificationsConfig',  #
     'wiki.plugins.images.apps.ImagesConfig',                #
-    'wiki.plugins.macros.apps.MacrosConfig',                # End Wiki Apps
+    'wiki.plugins.macros.apps.MacrosConfig',                #
+    'wiki.conf.settings.STORAGE_BACKEND',                   #
+    'wiki.plugins.images.settings.IMAGE_PATH',              # End Wiki Apps
     'dynowiki',
     'wikistuff',
 ]
